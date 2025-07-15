c43b047969cc83c53836770ec7b630e6995b93d9
kojo akomiah <kojoakomiah1@gmail.com>
2025-07-15 11:48:58 +0000

    Add files via upload

---
+++ age calculator.py
@@ -0,0 +1,7 @@
+print("welcome to the age calculator!!")
+the=str(input("what is your name?  "))
+a=int(input("what is your birth year? "))
+b=int(input("which year is this?  "))
+age=b-a
+print(f"you are {age} years old")
+print(type(age))
